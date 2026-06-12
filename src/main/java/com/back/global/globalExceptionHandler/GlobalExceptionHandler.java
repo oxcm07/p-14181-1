1ed73a662cf1e752041fe2167cd01ba6e0169335
oxcm07 <oxcm0727@gmail.com>
2026-06-12 14:02:00 +0900

    009

--- src/main/java/com/back/global/globalExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/back/global/globalExceptionHandler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.back.global.globalExceptionHandler;
 
 import com.back.global.rsData.RsData;
+import jakarta.validation.ConstraintViolationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Comparator;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
@@ -26,6 +28,31 @@ public class GlobalExceptionHandler {
                         "해당 데이터가 존재하지 않습니다."
                 ),
                 NOT_FOUND
+        );
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<RsData<Void>> handle(ConstraintViolationException ex) {
+        String message = ex.getConstraintViolations()
+                .stream()
+                .map(violation -> {
+                    String field = violation.getPropertyPath().toString().split("\\.", 2)[1];
+                    String[] messageTemplateBits = violation.getMessageTemplate()
+                            .split("\\.");
+                    String code = messageTemplateBits[messageTemplateBits.length - 2];
+                    String _message = violation.getMessage();
+
+                    return "%s-%s-%s".formatted(field, code, _message);
+                })
+                .sorted(Comparator.comparing(String::toString))
+                .collect(Collectors.joining("\n"));
+
+        return new ResponseEntity<>(
+                new RsData<>(
+                        "400-1",
+                        message
+                ),
+                BAD_REQUEST
         );
     }
 
