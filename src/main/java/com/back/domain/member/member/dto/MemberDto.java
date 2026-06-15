b6ab7ae44f735b6ef359b43322eee5e982d39f12
oxcm07 <oxcm0727@gmail.com>
2026-06-15 10:14:23 +0900

    019

--- src/main/java/com/back/domain/member/member/dto/MemberDto.java
+++ src/main/java/com/back/domain/member/member/dto/MemberDto.java
@@ -10,12 +10,6 @@ public record MemberDto (
         LocalDateTime modifyDate,
         String name
 ) {
-    public MemberDto(int id, LocalDateTime createDate, LocalDateTime modifyDate, String name) {
-        this.id = id;
-        this.createDate = createDate;
-        this.modifyDate = modifyDate;
-        this.name = name;
-    }
 
     public MemberDto(Member member) {
         this(
