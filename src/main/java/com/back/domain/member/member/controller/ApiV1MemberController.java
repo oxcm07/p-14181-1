88cb5c2045484726eb920d5f26b265a966a3c5ef
oxcm07 <oxcm0727@gmail.com>
2026-06-15 10:46:30 +0900

    020

--- src/main/java/com/back/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/com/back/domain/member/member/controller/ApiV1MemberController.java
@@ -4,6 +4,7 @@ package com.back.domain.member.member.controller;
 import com.back.domain.member.member.dto.MemberDto;
 import com.back.domain.member.member.entity.Member;
 import com.back.domain.member.member.service.MemberService;
+import com.back.global.exception.ServiceException;
 import com.back.global.rsData.RsData;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -36,6 +37,22 @@ public class ApiV1MemberController {
     ) {
     }
 
+    record MemberLoginRq(
+            @NotBlank
+            @Size(min = 2, max = 30)
+            String username,
+            @NotBlank
+            @Size(min = 2, max = 30)
+            String password
+    ) {
+    }
+
+    record MemberLoginResBody(
+            MemberDto item,
+            String apiKey
+    ) {
+    }
+
     @PostMapping
     @Operation(summary = "회원가입")
     public RsData<MemberDto> join(
@@ -52,6 +69,28 @@ public class ApiV1MemberController {
                 "201-1",
                 "%s님 환영합니다. 회원가입이 완료되었습니다.".formatted(member.getName()),
                 new MemberDto(member)
+        );
+    }
+
+    @PostMapping("/login")
+    @Operation(summary = "로그인")
+    public RsData<MemberLoginResBody> login(
+            @Valid @RequestBody MemberLoginRq reqBody
+    ) {
+        Member member = memberService.findByUsername(reqBody.username)
+                .orElseThrow(() -> new ServiceException("401-1", "존재하지 않는 아이디입니다."));
+
+        if (!member.getPassword().equals(reqBody.password)) {
+            throw new ServiceException("401-2", "비밀번호가 일치하지 않습니다.");
+        }
+
+        return new RsData<>(
+                "200-1",
+                "%s님 환영합니다.".formatted(member.getName()),
+                new MemberLoginResBody(
+                        new MemberDto(member),
+                        member.getApiKey()
+                )
         );
     }
 }
