031c1a97781f01cc1783bd7936438728e902e075
oxcm07 <oxcm0727@gmail.com>
2026-06-16 15:40:18 +0900

    052

--- src/main/java/com/back/global/rq/Rq.java
+++ src/main/java/com/back/global/rq/Rq.java
@@ -3,31 +3,73 @@ package com.back.global.rq;
 import com.back.domain.member.member.entity.Member;
 import com.back.domain.member.member.service.MemberService;
 import com.back.global.exception.ServiceException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class Rq {
     private final MemberService memberService;
     private final HttpServletRequest req;
+    private final HttpServletResponse resp;
 
     public Member getActor() {
-        String headerAuthorization = req.getHeader("Authorization");
+        String headerAuthorization = getHeader("Authorization", "");
 
-        if (headerAuthorization == null || headerAuthorization.isBlank())
+        String apiKey;
+
+        if (!headerAuthorization.isBlank()) {
+            if (!headerAuthorization.startsWith("Bearer ")) {
+                throw new ServiceException("401-2", "Authorization 헤더가 Bearer 형식이 아닙니다.");
+            }
+            apiKey = headerAuthorization.substring(7).trim();
+        } else {
+            apiKey = getCookieValue("apiKey", "");
+        }
+
+        if (apiKey.isBlank()) {
             throw new ServiceException("401-1", "로그인 후 이용해주세요.");
-
-        if (!headerAuthorization.startsWith("Bearer "))
-            throw new ServiceException("401-2", "Authorization 헤더가 Bearer 형식이 아닙니다.");
-
-        String apiKey = headerAuthorization.substring(7);
+        }
 
         Member member = memberService
                 .findByApiKey(apiKey)
                 .orElseThrow(() -> new ServiceException("401-3", "API 키가 유효하지 않습니다."));
 
         return member;
+    }
+
+    private String getHeader(String name, String defaultValue) {
+        return Optional
+                .ofNullable(req.getHeader(name))
+                .filter(headerValue -> !headerValue.isBlank())
+                .orElse(defaultValue);
+    }
+
+    private String getCookieValue(String name, String defaultValue) {
+        return Optional
+                .ofNullable(req.getCookies())
+                .flatMap(
+                        cookies ->
+                                Arrays.stream(cookies)
+                                        .filter(cookie -> cookie.getName().equals(name))
+                                        .map(Cookie::getValue)
+                                        .filter(value -> !value.isBlank())
+                                        .findFirst()
+                )
+                .orElse(defaultValue);
+    }
+
+    public void setCookie(String name, String value) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+
+        resp.addCookie(cookie);
     }
 }
--- src/test/java/com/back/domain/member/member/controller/ApiV1MemberControllerTest.java
+++ src/test/java/com/back/domain/member/member/controller/ApiV1MemberControllerTest.java
@@ -2,6 +2,7 @@ package com.back.domain.member.member.controller;
 
 import com.back.domain.member.member.entity.Member;
 import com.back.domain.member.member.service.MemberService;
+import jakarta.servlet.http.Cookie;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -91,7 +93,15 @@ public class ApiV1MemberControllerTest {
                 .andExpect(jsonPath("$.data.item.createDate").value(Matchers.startsWith(member.getCreateDate().toString().substring(0, 20))))
                 .andExpect(jsonPath("$.data.item.modifyDate").value(Matchers.startsWith(member.getModifyDate().toString().substring(0, 20))))
                 .andExpect(jsonPath("$.data.item.name").value(member.getName()))
-                .andExpect(jsonPath("$.data.apiKey").value(member.getApiKey()));
+                .andExpect(jsonPath("$.data.apiKey").value(member.getApiKey()))
+                .andExpect(
+                        result -> {
+                            Cookie apiKeyCookie = result.getResponse().getCookie("apiKey");
+                            assertThat(apiKeyCookie.getValue()).isEqualTo(member.getApiKey());
+                            assertThat(apiKeyCookie.getPath()).isEqualTo("/");
+                            assertThat(apiKeyCookie.isHttpOnly()).isTrue();
+                        }
+                );
     }
 
     @Test
@@ -104,6 +114,34 @@ public class ApiV1MemberControllerTest {
                 .perform(
                         get("/api/v1/members/me")
                                 .header("Authorization", "Bearer " + actorApiKey)
+                )
+                .andDo(print());
+
+        Member member = memberService.findByUsername("user1").get();
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1MemberController.class))
+                .andExpect(handler().methodName("me"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.resultCode").value("200-1"))
+                .andExpect(jsonPath("$.msg").value("%s님의 정보입니다.".formatted(member.getName())))
+                .andExpect(jsonPath("$.data").exists())
+                .andExpect(jsonPath("$.data.id").value(member.getId()))
+                .andExpect(jsonPath("$.data.createDate").value(Matchers.startsWith(member.getCreateDate().toString().substring(0, 20))))
+                .andExpect(jsonPath("$.data.modifyDate").value(Matchers.startsWith(member.getModifyDate().toString().substring(0, 20))))
+                .andExpect(jsonPath("$.data.name").value(member.getName()));
+    }
+
+    @Test
+    @DisplayName("내 정보, with apiKey Cookie")
+    void t4() throws Exception {
+        Member actor = memberService.findByUsername("user1").get();
+        String actorApiKey = actor.getApiKey();
+
+        ResultActions resultActions = mvc
+                .perform(
+                        get("/api/v1/members/me")
+                                .cookie(new Cookie("apiKey", actorApiKey))
                 )
                 .andDo(print());
 
