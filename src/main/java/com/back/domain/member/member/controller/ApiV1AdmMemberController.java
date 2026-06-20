e8878fb295280d2f81093ecb2c735f74b9d37e6e
oxcm07 <oxcm0727@gmail.com>
2026-06-20 23:28:31 +0900

    095

--- src/main/java/com/back/domain/member/member/controller/ApiV1AdmMemberController.java
+++ src/main/java/com/back/domain/member/member/controller/ApiV1AdmMemberController.java
@@ -3,10 +3,13 @@ package com.back.domain.member.member.controller;
 import com.back.domain.member.member.dto.MemberWithUsernameDto;
 import com.back.domain.member.member.entity.Member;
 import com.back.domain.member.member.service.MemberService;
+import com.back.global.exception.ServiceException;
+import com.back.global.rq.Rq;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,13 +22,35 @@ import java.util.List;
 @SecurityRequirement(name = "bearerAuth")
 public class ApiV1AdmMemberController {
     private final MemberService memberService;
+    private final Rq rq;
 
     @GetMapping
     public List<MemberWithUsernameDto> getItems() {
+        Member actor = rq.getActor();
+
+        if( !actor.isAdmin()){
+            throw new ServiceException("403-1", "권한이 없습니다.");
+        }
+
         List<Member> members = memberService.findAll();
 
         return members.stream()
                 .map(MemberWithUsernameDto::new)
                 .toList();
+    }
+
+    @GetMapping("/{id}")
+    public MemberWithUsernameDto getItem(
+            @PathVariable int id
+    ) {
+        Member actor = rq.getActor();
+
+        if( !actor.isAdmin()){
+            throw new ServiceException("403-1", "권한이 없습니다.");
+        }
+
+        Member member = memberService.findById(id).get();
+
+        return new MemberWithUsernameDto(member);
     }
 }
--- src/test/java/com/back/domain/member/member/controller/ApiV1AdmMemberControllerTest.java
+++ src/test/java/com/back/domain/member/member/controller/ApiV1AdmMemberControllerTest.java
@@ -32,9 +32,13 @@ public class ApiV1AdmMemberControllerTest {
     @Test
     @DisplayName("다건조회")
     void t1() throws Exception {
+        Member actor = memberService.findByUsername("admin").get();
+        String actorApiKey = actor.getApiKey();
+
         ResultActions resultActions = mvc
                 .perform(
                         get("/api/v1/adm/members")
+                                .header("Authorization", "Bearer " + actorApiKey)
                 )
                 .andDo(print());
 
@@ -55,5 +59,76 @@ public class ApiV1AdmMemberControllerTest {
                     .andExpect(jsonPath("$[%d].name".formatted(i)).value(member.getName()))
                     .andExpect(jsonPath("$[%d].username".formatted(i)).value(member.getUsername()));
         }
+    }
+
+    @Test
+    @DisplayName("다건조회, without permission")
+    void t3() throws Exception {
+        Member actor = memberService.findByUsername("user1").get();
+        String actorApiKey = actor.getApiKey();
+
+        ResultActions resultActions = mvc
+                .perform(
+                        get("/api/v1/adm/members")
+                                .header("Authorization", "Bearer " + actorApiKey)
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1AdmMemberController.class))
+                .andExpect(handler().methodName("getItems"))
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("$.resultCode").value("403-1"))
+                .andExpect(jsonPath("$.msg").value("권한이 없습니다."));
+    }
+
+    @Test
+    @DisplayName("단건조회")
+    void t2() throws Exception {
+        Member actor = memberService.findByUsername("admin").get();
+        String actorApiKey = actor.getApiKey();
+        int id = 1;
+
+        ResultActions resultActions = mvc
+                .perform(
+                        get("/api/v1/adm/members/" + id)
+                                .header("Authorization", "Bearer " + actorApiKey)
+                )
+                .andDo(print());
+
+        Member member = memberService.findById(id).get();
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1AdmMemberController.class))
+                .andExpect(handler().methodName("getItem"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(member.getId()))
+                .andExpect(jsonPath("$.createDate").value(Matchers.startsWith(member.getCreateDate().toString().substring(0, 20))))
+                .andExpect(jsonPath("$.modifyDate").value(Matchers.startsWith(member.getModifyDate().toString().substring(0, 20))))
+                .andExpect(jsonPath("$.username").value(member.getUsername()))
+                .andExpect(jsonPath("$.name").value(member.getName()));
+    }
+
+    @Test
+    @DisplayName("단건조회, without permission")
+    void t4() throws Exception {
+        Member actor = memberService.findByUsername("user1").get();
+        String actorApiKey = actor.getApiKey();
+
+        int id = 1;
+
+        ResultActions resultActions = mvc
+                .perform(
+                        get("/api/v1/adm/members/" + id)
+                                .header("Authorization", "Bearer " + actorApiKey)
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1AdmMemberController.class))
+                .andExpect(handler().methodName("getItem"))
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("$.resultCode").value("403-1"))
+                .andExpect(jsonPath("$.msg").value("권한이 없습니다."));
     }
 }
