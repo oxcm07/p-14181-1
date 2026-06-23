26134060de37eb441b5aa3f9e436cc123c854331
oxcm07 <oxcm0727@gmail.com>
2026-06-23 10:56:32 +0900

    129

--- src/main/java/com/back/global/security/SecurityConfig.java
+++ src/main/java/com/back/global/security/SecurityConfig.java
@@ -49,9 +49,11 @@ public class SecurityConfig {
                                         HeadersConfigurer.FrameOptionsConfig::sameOrigin
                                 )
                 )
-                .csrf(
-                        AbstractHttpConfigurer::disable
-                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(AbstractHttpConfigurer::disable)
                 .addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(
                         exceptionHandling -> exceptionHandling
